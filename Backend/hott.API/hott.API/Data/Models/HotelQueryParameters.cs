5f7afeaffaeb84ce015e5fa0fb8a22f737e269c6
EgorSalahov <egorsalahov2@gmail.com>
2026-03-27 13:36:50 +0300

    homework(4): реализован API для туров

---
+++ Backend/hott.API/hott.API/Data/Models/HotelQueryParameters.cs
@@ -0,0 +1,18 @@
+﻿public class HotelQueryParameters
+{
+    public string? Departure { get; set; }
+    public string? Country { get; set; }
+    public string? Dates { get; set; }
+    public string? Nights { get; set; }
+    public string? Tourists { get; set; } // Например: "2 взрослых 1 реб"
+
+    // Параметры удобств
+    public bool? Amenities_tv { get; set; }
+    public bool? Amenities_slippers { get; set; }
+    public bool? Amenities_shower { get; set; }
+    public bool? Amenities_airConditioner { get; set; }
+    public bool? Amenities_miniBar { get; set; }
+    public bool? Amenities_phone { get; set; }
+    public bool? Amenities_heating { get; set; }
+    public bool? Amenities_wifi { get; set; }
+}
